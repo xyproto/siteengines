73399497fcb67e95b626cc00793c4afd7ac9f19b
Alexander Rødseth <rodseth@gmail.com>
2013-04-30 16:15:32 +0200

    Added week view

--- ftlsengine.go
+++ ftlsengine.go
@@ -1,6 +1,8 @@
 package siteengines
 
 import (
+	"strconv"
+	"strings"
 	"time"
 
 	. "github.com/xyproto/browserspeak"
@@ -82,10 +84,71 @@ func (we *FTLSEngine) ServePages(basecp BaseCP, menuEntries MenuEntries) {
 	web.Get("/css/ftls.css", we.GenerateCSS(ftlsCP.ColorScheme))            // CSS that is specific for ftls pages
 }
 
+// TODO: Find a more clever way to translate everything
+func MonthName(month int, language string) string {
+	var names []string
+
+	// TODO: Return err or "" instead of panic?
+	if month == 0 {
+		panic("Invalid month number: 0")
+	} else if month >= 13 {
+		panic("Month number too high: " + strconv.Itoa(month))
+	}
+
+	if language == "no" {
+		names = []string{"NIL", "januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "september", "oktober", "november", "desember"}
+	} else {
+		names = []string{"NIL", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+	}
+
+	return names[month]
+}
+
+func RenderWeekFrom(year, month, startday int) string {
+	retval := ""
+	retval += "<table>"
+
+	// Headers
+	retval += "<tr>"
+	for day := startday; day < (startday + 7); day++ {
+		retval += "<td><b>" + Num2dd(day) + ". " + MonthName(month, "no") + "</b></td>"
+	}
+	retval += "</tr>"
+
+	// Each row is an hour
+	for hour := 8; hour < 22; hour++ {
+		retval += "<tr>"
+		// TODO: Use time/date functions for adding days instead, these months can go to day 37...
+		// Each column is a day
+		for day := startday; day < (startday + 7); day++ {
+			retval += "<td>kl. " + Num2dd(hour) + ":00</td>"
+		}
+		retval += "</tr>"
+	}
+
+	retval += "</table>"
+	return retval
+}
+
+// Convert from a number to a double digit string
+func Num2dd(num int) string {
+	s := strconv.Itoa(num)
+	if len(s) == 2 {
+		return s
+	}
+	return "0" + s
+}
+
 func (we *FTLSEngine) GenerateShowFTLS() WebHandle {
-	return func(ctx *web.Context, weeknr string) string {
+	return func(ctx *web.Context, date string) string {
+		ymd := strings.Split(date, "-")
+		// TODO: Error checking when splitting and converting + user input cleanup
+		year, _ := strconv.Atoi(ymd[0])
+		month, _ := strconv.Atoi(ymd[1])
+		day, _ := strconv.Atoi(ymd[2])
 		retval := ""
-		retval += "<h1>Hi</h1>"
+		retval += "<h1>En uke fra " + strconv.Itoa(year) + "-" + Num2dd(month) + "-" + Num2dd(day) + "</h1>"
+		retval += RenderWeekFrom(year, month, day)
 		retval += BackButton()
 		return retval
 	}
@@ -93,8 +156,9 @@ func (we *FTLSEngine) GenerateShowFTLS() WebHandle {
 
 func (we *FTLSEngine) GenerateFTLSRedirect() SimpleContextHandle {
 	return func(ctx *web.Context) string {
-		// TODO: Redirect to the current week nr
-		ctx.SetHeader("Refresh", "0; url=/ftls/0", true)
+		t := time.Now()
+		// Redirect to the current date on the form yyyy-mm-dd
+		ctx.SetHeader("Refresh", "0; url=/ftls/"+t.String()[:10], true)
 		return ""
 	}
 }
@@ -103,6 +167,12 @@ func (we *FTLSEngine) GenerateCSS(cs *ColorScheme) SimpleContextHandle {
 	return func(ctx *web.Context) string {
 		ctx.ContentType("css")
 		return `
+.even {
+	background-color: "a0a0a0;
+}
+.odd {
+	background-color: #f0f0f0;
+}
 .yes {
 	background-color: #90ff90;
 	color: black;
@@ -110,6 +180,16 @@ func (we *FTLSEngine) GenerateCSS(cs *ColorScheme) SimpleContextHandle {
 .no {
 	background-color: #ff9090;
 	color: black;
+}
+table {
+	border-collapse: collapse;
+	padding: 1em;
+	margin-top: 1.5em;
+	margin-bottom: 1em;
+}
+table, th, tr, td {
+	border: 1px solid black;
+	padding: 1em;
 }
 
 .username:link { color: green; }
@@ -126,9 +206,15 @@ func (we *FTLSEngine) GenerateCSS(cs *ColorScheme) SimpleContextHandle {
 .darkgrey:hover { color: #404040; }
 .darkgrey:active { color: #404040; }
 
-#ftlsText {
-	background-color: white;
-}
+.somewhatcareful:link { color: #e09000; }
+.somewhatcareful:visited { color: #e09000; }
+.somewhatcareful:hover { color: #e09000; }
+.somewhatcareful:active { color: #e09000; }
+
+.careful:link { color: #e00000; }
+.careful:visited { color: #e00000; }
+.careful:hover { color: #e00000; }
+.careful:active { color: #e00000; }
 
 `
 		//
