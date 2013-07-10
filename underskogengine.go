b49d72c9a5c7f111a3e24ae3a199c6ed4c340607
Alexander Rødseth <rodseth@gmail.com>
2013-07-10 17:17:41 +0200

    Added the beginnings of an engine for Underskog

---
+++ underskogengine.go
@@ -0,0 +1,100 @@
+package siteengines
+
+import (
+	"github.com/hoisie/web"
+	. "github.com/xyproto/browserspeak"
+	. "github.com/xyproto/genericsite"
+	"github.com/xyproto/simpleredis"
+)
+
+type UnderskogEngine struct {
+	//plans *simpleredis.HashMap
+
+	pool      *simpleredis.ConnectionPool // A connection pool for Redis
+	userState *UserState
+}
+
+func NewUnderskogEngine(userState *UserState) *UnderskogEngine {
+	pool := userState.GetPool()
+	return &UnderskogEngine{pool, userState}
+}
+
+func (ue *UnderskogEngine) ServePages(basecp BaseCP, menuEntries MenuEntries) {
+	underskogCP := basecp(ue.userState)
+
+	underskogCP.ContentTitle = "Mosebark"
+	underskogCP.ExtraCSSurls = append(underskogCP.ExtraCSSurls, "/css/mosebark.css")
+
+	tvgf := DynamicMenuFactoryGenerator(menuEntries)
+	tvg := tvgf(ue.userState)
+
+	web.Get("/mosebark", underskogCP.WrapWebHandle(ue.GenerateMessages(), tvg))
+	web.Get("/css/mosebark.css", ue.GenerateCSS(underskogCP.ColorScheme))
+}
+
+func (ue *UnderskogEngine) GenerateMessages() WebHandle {
+	return func(ctx *web.Context, userdate string) string {
+		retval := ""
+		retval += "<h1>MESSAGES</h1>"
+		retval += BackButton()
+		return retval
+	}
+}
+
+func (ue *UnderskogEngine) GenerateCSS(cs *ColorScheme) SimpleContextHandle {
+	return func(ctx *web.Context) string {
+		ctx.ContentType("css")
+		return `
+.even {
+	background-color: "a0a0a0;
+}
+.odd {
+	background-color: #f0f0f0;
+}
+.yes {
+	background-color: #90ff90;
+	color: black;
+}
+.no {
+	background-color: #ff9090;
+	color: black;
+}
+table {
+	border-collapse: collapse;
+	padding: 1em;
+	margin-top: 1.5em;
+	margin-bottom: 1em;
+}
+table, th, tr, td {
+	border: 1px solid black;
+	padding: 1em;
+}
+
+.username:link { color: green; }
+.username:visited { color: green; }
+.username:hover { color: green; }
+.username:active { color: green; }
+
+.whitebg {
+	background-color: white;
+}
+
+.darkgrey:link { color: #404040; }
+.darkgrey:visited { color: #404040; }
+.darkgrey:hover { color: #404040; }
+.darkgrey:active { color: #404040; }
+
+.somewhatcareful:link { color: #e09000; }
+.somewhatcareful:visited { color: #e09000; }
+.somewhatcareful:hover { color: #e09000; }
+.somewhatcareful:active { color: #e09000; }
+
+.careful:link { color: #e00000; }
+.careful:visited { color: #e00000; }
+.careful:hover { color: #e00000; }
+.careful:active { color: #e00000; }
+
+`
+		//
+	}
+}
